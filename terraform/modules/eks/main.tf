c267cdb2f6032b75cef1a519871b5a4a8b5a510d
isaac <isaacdennis940@gmail.com>
2025-12-01 21:02:42 -0500

    push

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -13,7 +13,12 @@ resource "aws_eks_node_group" "nodes" {
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.private_subnets
 
-  scaling_config { desired_size = 1, min_size = 1, max_size = 1 }
+  scaling_config {
+  desired_size = 1
+  min_size     = 1
+  max_size     = 1
+}
+
 
   instance_types = ["t3.small"]
 
