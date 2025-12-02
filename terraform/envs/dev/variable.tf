58b774904b5cc34ca160dfbddf674acecf7e1cba
isaac <isaacdennis940@gmail.com>
2025-12-01 21:30:59 -0500

    push

--- terraform/envs/dev/variable.tf
+++ terraform/envs/dev/variable.tf
@@ -1,7 +1,9 @@
 variable "cluster_role_arn" {
   type = string
+  default = "arn:aws:iam::123456789012:role/EKSClusterRole"
 }
 
 variable "node_role_arn" {
   type = string
+  default = "arn:aws:iam::123456789012:role/EKSNodeGroupRole"
 }
