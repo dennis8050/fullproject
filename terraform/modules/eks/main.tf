33c4d629844bd5d7e5cae3896daf3ca8e909fdee
isaac <isaacdennis940@gmail.com>
2026-01-05 15:52:16 -0500

    done

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -27,7 +27,7 @@ resource "aws_eks_node_group" "nodes" {
   scaling_config {
     desired_size = 1
     min_size     = 1
-    max_size     = 1
+    max_size     = 2
   }
 
   instance_types = ["t3.small"]
