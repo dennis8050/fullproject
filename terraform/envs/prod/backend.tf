5befc028798c04b8163c0b6d2b2d7b431a7b38fe
isaac <isaacdennis940@gmail.com>
2026-02-07 00:31:44 -0500

    new

--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "terraformstatefile-dev"
-    key          = "dev/terraform.tfstate"
+    bucket       = "terraformstatefile-prod"
+    key          = "prod/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
