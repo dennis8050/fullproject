27c984b963d6f21b0adc2523071cbb714092ddfc
isaac <isaacdennis940@gmail.com>
2026-02-07 01:07:02 -0500

    new

--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -1,6 +1,8 @@
-# backend "s3" {
-#   bucket         = "terraformstatefile-prod"
-#   key            = "prod/terraform.tfstate"
-#   region         = "us-east-1"
-#   dynamodb_table = "terraform-lock"
-# }
+terraform {
+  backend "s3" {
+    bucket         = "terraformstatefile-prod"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
