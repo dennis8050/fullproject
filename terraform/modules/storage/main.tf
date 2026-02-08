b13c8131b1b31b552ce1e57f589f010abfaac9da
isaac <isaacdennis940@gmail.com>
2026-02-07 21:01:57 -0500

    new

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,10 +1,15 @@
 
+resource "kubernetes_namespace_v1" "this" {
+  metadata {
+    name = "monitoring"
+  }
+}
 
 resource "kubernetes_persistent_volume_claim_v1" "app_pvc" {
 
   metadata {
     name      = "${var.app_name}-pvc"
-    namespace = var.namespace
+    namespace =kubernetes_namespace_v1.this.metadata[0].name
   }
   spec {
     access_modes = ["ReadWriteOnce"]
