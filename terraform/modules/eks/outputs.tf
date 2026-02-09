6904b5f15850ac8e4bb7c9deb76a297c5a2c85f8
isaac <isaacdennis940@gmail.com>
2026-02-08 23:15:43 -0500

    new

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -1,3 +1,6 @@
+# -----------------------------
+# EKS Cluster Outputs
+# -----------------------------
 output "cluster_name" {
   value = aws_eks_cluster.this.name
 }
@@ -10,12 +13,18 @@ output "cluster_certificate" {
   value = aws_eks_cluster.this.certificate_authority[0].data
 }
 
-output "oidc_provider_arn" {
-  value = aws_iam_openid_connect_provider.this[0].arn
+# -----------------------------
+# OIDC Provider Outputs
+# -----------------------------
+# Use a data source to safely reference an existing OIDC provider
+data "aws_iam_openid_connect_provider" "eks" {
+  url = "https://oidc.eks.${var.aws_region}.amazonaws.com/id/${module.eks.cluster_oidc_id}"
 }
 
+output "oidc_provider_arn" {
+  value = data.aws_iam_openid_connect_provider.eks.arn
+}
 
 output "oidc_provider_url" {
-  value = aws_iam_openid_connect_provider.this[0].url
+  value = data.aws_iam_openid_connect_provider.eks.url
 }
-
