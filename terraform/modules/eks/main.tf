dd7313315d29c363d354e7f5beda4d69eb00c957
isaac <isaacdennis940@gmail.com>
2026-02-08 21:47:32 -0500

    new

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -89,3 +89,19 @@ resource "aws_eks_node_group" "nodes" {
     aws_iam_role_policy_attachment.node_registry
   ]
 }
+############################
+# EKS oidc creation
+############################
+data "aws_eks_cluster" "this" {
+  name = aws_eks_cluster.eks.name
+}
+
+data "tls_certificate" "eks" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "this" {
+  url             = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+}
