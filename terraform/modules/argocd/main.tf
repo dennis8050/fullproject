05275a4c4b757a2fcc6da07b42c38763d1765b22
isaac <isaacdennis940@gmail.com>
2026-02-10 18:44:26 -0500

    new

--- terraform/modules/argocd/main.tf
+++ terraform/modules/argocd/main.tf
@@ -2,6 +2,7 @@
 # Namespace for ArgoCD
 # -----------------------------
 resource "kubernetes_namespace_v1" "argocd" {
+  provider = kubernetes.eks
   metadata {
     name = var.namespace
     labels = {
