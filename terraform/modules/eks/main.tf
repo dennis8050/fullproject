cd1daaa833dfc10825e40be357bdb634874ccf5e
isaac <isaacdennis940@gmail.com>
2026-02-09 20:10:40 -0500

    new

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -96,7 +96,9 @@ resource "aws_eks_node_group" "nodes" {
 data "aws_eks_cluster" "this" {
   name = aws_eks_cluster.this.name
 }
-
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
 data "tls_certificate" "eks" {
   url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
