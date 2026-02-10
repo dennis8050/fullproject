66c1cf9576a689019498795fb4a06785ceec7a30
isaac <isaacdennis940@gmail.com>
2026-02-09 19:35:41 -0500

    new

--- terraform/envs/prod/providers.tf
+++ terraform/envs/prod/providers.tf
@@ -1,43 +1,28 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.31.0"   # match your AWS provider
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.25.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.10.0"
-    }
-  }
-
-  required_version = ">= 1.14.4"  # match your Terraform version
-}
-
 provider "aws" {
   region = var.aws_region
 }
 
-
-
+# Token only (this is correct)
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
 
+# Kubernetes provider (USE MODULE OUTPUTS)
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate)
   token                  = data.aws_eks_cluster_auth.this.token
+
   
 }
 
+# Helm provider (USE MODULE OUTPUTS)
 provider "helm" {
   kubernetes = {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate)
     token                  = data.aws_eks_cluster_auth.this.token
   }
+
+ 
 }
