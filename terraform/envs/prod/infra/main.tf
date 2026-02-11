3775c088310b826b58ced046db1e8aa5551f7130
isaac <isaacdennis940@gmail.com>
2026-02-10 21:15:59 -0500

    new

--- terraform/envs/prod/infra/main.tf
+++ terraform/envs/prod/infra/main.tf
@@ -3,20 +3,24 @@ provider "aws" {
 }
 
 module "vpc" {
-  source          = "../../modules/vpc"
+  source          = "../../../modules/vpc"
   vpc_cidr        = var.vpc_cidr
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
   azs             = var.azs
+  env = var.env
+  project_name = var.project_name
+  
 }
 
 module "eks" {
-  source               = "../../modules/eks"
+  source               = "../../../modules/eks"
   cluster_name         = "${var.project_name}-${var.env}"
   private_subnets      = module.vpc.private_subnets
   node_type            = var.eks_node_type
   node_count           = var.eks_node_count
   create_oidc_provider = var.create_oidc_provider
-
+aws_region = var.aws_region
+vpc_id = module.vpc
   depends_on = [module.vpc]
 }
--- terraform/envs/prod/platform/main.tf
+++ terraform/envs/prod/platform/main.tf
@@ -4,12 +4,40 @@ module "argocd" {
 }
 
 module "monitoring" {
-  source    = "../../modules/monitoring"
-  namespace = "monitoring"
+  source    = "../../../modules/monitoring"
+     env       = var.env
+  namespace = "monitoring-${var.env}"
+  sso_client_id = var.sso_client_id
+  sso_client_secret = var.sso_client_secret
+  sso_auth_url = var.sso_auth_url
+  sso_token_url = var.sso_token_url
+  sso_api_url = var.sso_api_url
+  monitoring_domain = var.monitoring_domain
+  ssl_certificate_arn = var.ssl_certificate_arn
+
+
+ depends_on = [
+  module.eks,
+  module.loki_storage
+ 
+]
+ loki_existing_claim = module.loki_storage.pvc_name
+
 }
 
 module "irsa" {
-  source            = "../../modules/irsa-role"
-  cluster_name      = data.terraform_remote_state.infra.outputs.cluster_name
-  oidc_provider_arn = data.terraform_remote_state.infra.outputs.oidc_provider_arn
+  source            = "../../../modules/irsa-role"
+  
+  cluster_name        = module.eks.cluster_name
+  env                 = var.env
+  oidc_provider_arn   = module.eks.oidc_provider_arn
+  oidc_provider_url   = module.eks.oidc_provider_url
+  
+  depends_on = [module.eks]
+}
+module "eks-addons" {
+  source = "../../../modules/eks-addons"
+  
+  # Pass the EBS IRSA role ARN to the Helm module
+  ebs_csi_irsa_arn = module.irsa-role.ebs_csi_irsa_arn
 }
