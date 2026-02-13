003b8bacfde2558c15300aa0c78bcc2783355361
isaac <isaacdennis940@gmail.com>
2026-02-13 18:00:36 -0500

    new

--- terraform/modules/storage_pvc_loki/main.tf
+++ terraform/modules/storage_pvc_loki/main.tf
@@ -1,20 +1,3 @@
-#below is aws storage class for loki
-resource "kubernetes_storage_class_v1" "gp3" {
-  metadata {
-    name = "gp3"
-  }
-
-  storage_provisioner = "ebs.csi.aws.com"
-
-  parameters = {
-    type = "gp3"
-  }
-
-  reclaim_policy      = "Delete"
-  volume_binding_mode = "WaitForFirstConsumer"
-
-  allow_volume_expansion = true
-}
 
 
 
