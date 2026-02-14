d9209adb825ae80c9c66b1357a4295fdd457bdda
isaac <isaacdennis940@gmail.com>
2026-02-13 20:18:29 -0500

    new

--- terraform/modules/eks-addons/variables.tf
+++ terraform/modules/eks-addons/variables.tf
@@ -1,5 +1,8 @@
 
-variable "ebs_csi_irsa_arn" {
-  type        = string
-  description = "IAM role ARN for EBS CSI driver service account"
+
+variable "cluster_name" {
+  
+}
+variable "ebs_csi_role_arn" {
+  
 }
