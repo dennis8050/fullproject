0258597c1919ba58dae366ad4468b37533b29ec6
isaac <isaacdennis940@gmail.com>
2026-02-14 00:41:33 -0500

    new

--- terraform/modules/storage_pvc_loki/main.tf
+++ terraform/modules/storage_pvc_loki/main.tf
@@ -12,7 +12,7 @@ resource "kubernetes_namespace_v1" "monitoring" {
 # ----------------------------
 resource "kubernetes_storage_class_v1" "gp3_loki" {
   metadata {
-    name = "gp3_loki"
+    name = "gp3-loki"
   }
 
   storage_provisioner     = "ebs.csi.aws.com"
