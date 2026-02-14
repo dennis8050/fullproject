2725aed05703de200b9b3c4c04227ccdb0463d04
isaac <isaacdennis940@gmail.com>
2026-02-14 01:27:09 -0500

    new

--- terraform/modules/storage_pvc_loki/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "pvc_name" {
-  value = kubernetes_persistent_volume_claim_v1.app_pvc.metadata[0].name
-}
-
-output "storage_class_name" {
-  value = kubernetes_storage_class_v1.gp3_loki.metadata[0].name
-}
